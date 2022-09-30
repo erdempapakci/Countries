151d2af4e963908b1e8151415a5b6c3750269982
erdempapakci <erdmppkc@gmail.com>
2022-09-30 21:02:33 +0300

    App init

---
+++ CountriesReworkTests/CountriesReworkTests.swift
@@ -0,0 +1,29 @@
+//
+//  CountriesReworkTests.swift
+//  CountriesReworkTests
+//
+//  Created by Erdem Papakçı on 27.09.2022.
+//
+
+import XCTest
+@testable import CountriesRework
+
+final class CountriesReworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+ 
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
